240626ae349f9080b8fd6fd86764922dd5f242bf
HuyDung <nguyenhdung2006@gmail.com>
2026-06-09 09:11:17 +0700

    Revert "Fix vocabulary custom select dropdowns"
    This reverts commit dcde71a47cea1b2593cbdd0d9d02e40e357d4056.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -74,13 +74,6 @@ updateDifficulty();
 
 renderTable();
 
-/* Enhance vocabulary selects with custom dark dropdowns */
-enhanceSelect(document.getElementById("posInput"));
-enhanceSelect(document.getElementById("levelInput"));
-enhanceSelect(document.getElementById("filterPos"));
-enhanceSelect(document.getElementById("filterTag"));
-enhanceSelect(document.getElementById("filterMastery"));
-
 /* ENTER ADD WORD */
 
 engInput.addEventListener("keypress", function (e) {
